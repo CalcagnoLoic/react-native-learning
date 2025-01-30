4c5d638eabe0ef744a3c04f1e0171dd1ee823384
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2025-01-30 13:45:18 +0100

    ajout des onglets

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -26,7 +26,8 @@ export default function RootLayout() {
 
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{headerShown: false}} />
     </Stack>
   );
 }
---
+++ definitions.ts
@@ -0,0 +1,8 @@
+import { ImageSourcePropType } from "react-native";
+
+export type tabsIcons = {
+  icon: ImageSourcePropType ;
+  color: string;
+  name: string;
+  focused: boolean;
+};
